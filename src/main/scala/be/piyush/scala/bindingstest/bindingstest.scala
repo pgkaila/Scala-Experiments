219b72e7d567476a44e5619900cc1b74fef5d8a1
Piyush <pgkaila@gmail.com>
2016-03-13 19:12:35 +0530

    Bindings test

---
+++ src/main/scala/be/piyush/scala/bindingstest/bindingstest.scala
@@ -0,0 +1,50 @@
+package be.piyush.scala.bindingstest
+
+/**
+ * @Author Piyush Kaila
+ */
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    println("testSamePackage")
+    testSamePackage()
+    println("\ntestWildcardImport")
+    testWildcardImport()
+    println("\ntestExplicitImport")
+    testExplicitImport()
+    println("\ntestInlineDefinition")
+    testInlineDefinition()
+  }
+
+  def testSamePackage(): Unit ={
+    println(x)
+  }
+
+  def testWildcardImport(): Unit = {
+    import Wildcard._
+    println(x)
+  }
+
+  def testExplicitImport(): Unit = {
+    import Explicit.x
+    import Wildcard._
+    println(x)
+  }
+
+  def testInlineDefinition(): Unit = {
+    def x = "Object x defined inline"
+    import Explicit.x
+    import Wildcard._
+    println(x)
+  }
+
+  object Wildcard {
+    def x = "Object bound from Wildcard object"
+  }
+
+  object Explicit {
+    def x = "Object bound from Explicit object"
+  }
+
+
+}
---
+++ src/main/scala/be/piyush/scala/bindingstest/externalbindings.scala
@@ -0,0 +1,8 @@
+package be.piyush.scala.bindingstest
+
+/**
+ * @Author Piyush Kaila
+ */
+object x {
+  override def toString = "Externally bound object from package bindingtest"
+}
