560c9163efa03833b432c4bbc389ecb6b06d3f84
Piyush <pgkaila@gmail.com>
2016-03-13 18:43:09 +0530

    Using Traits as Mixins

---
+++ src/main/scala/be/piyush/scala/tdd/Mixins.scala
@@ -0,0 +1,29 @@
+package be.piyush.scala.tdd
+
+/**
+ * @Author Piyush Kaila
+ */
+
+trait Singer {
+  def sing {
+    println( "singing ..." )
+  }
+}
+
+class Bird extends Singer
+
+class Person {
+  def tell {
+    println( "here's a little story ..." )
+  }
+}
+
+object Mixins {
+  def main(args: Array[String]) {
+    val singingPerson = new Person with Singer // Person who can sing.
+    singingPerson.sing
+
+    val person = new Person
+    person.tell
+  }
+}
